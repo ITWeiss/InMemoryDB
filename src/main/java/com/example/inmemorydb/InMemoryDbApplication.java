f063acef30a332070853b890147c2f7ab57339ca
ITWeiss <gottfried98@bk.ru>
2025-04-05 12:44:48 +0700

    Initial commit

---
+++ src/main/java/com/example/inmemorydb/InMemoryDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.inmemorydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryDbApplication.class, args);
+	}
+
+}
